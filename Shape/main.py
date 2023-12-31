aa980096132379f55165fe31b7485b60257cf21a
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-12-31 13:27:09 +0300

    Colors and Shape

---
+++ Shape/main.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+canvas = np.zeros((500,500,3), np.uint8) + 255
+
+#lines
+cv2.line(canvas, (100,100), (300,300), (0,0,255), thickness=5)
+cv2.line(canvas, (300,350), (400,500), (255,0,0), 8)
+
+#rectangles
+cv2.rectangle(canvas, (30,30), (60,60), (0,255,0), 4)
+cv2.rectangle(canvas, (50,50), (80,80), (0,255,0), -1)
+
+cv2.imshow("window", canvas)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

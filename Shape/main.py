98b2ae258b6574f8b88e7de36eb40d6ac0b5dedf
Muhammed Ozmen <m.ozmenn34@gmail.com>
2023-12-31 19:51:50 +0300

    Shapes

--- Shape/main.py
+++ Shape/main.py
@@ -11,6 +11,18 @@ cv2.line(canvas, (300,350), (400,500), (255,0,0), 8)
 cv2.rectangle(canvas, (30,30), (60,60), (0,255,0), 4)
 cv2.rectangle(canvas, (50,50), (80,80), (0,255,0), -1)
 
+#circles
+cv2.circle(canvas, (100,100), 100, (255,0,0), 4)
+
+#triangles
+u1=(300,500)
+u2=(300,400)
+u3=(500,300)
+
+cv2.line(canvas, u1, u2, (0,0,0), 4)
+cv2.line(canvas, u2, u3, (0,0,0), 4)
+cv2.line(canvas, u1, u3, (0,0,0), 4)
+
 cv2.imshow("window", canvas)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
